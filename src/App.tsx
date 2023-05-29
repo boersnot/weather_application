227d52813cdd62d626ea062f9179fa424324cacf
Muf Fie <joko@bilkoek.com>
2023-05-29 14:12:29 +1000

    1st commit weather app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
 function App() {
+  React.useEffect(() => {
+    axios.get("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=fe4feefa8543e06d4f3c66d92c61b69c").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+       
       </header>
     </div>
   );
