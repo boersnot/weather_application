f16048092bc1c971ced9c0e10cad3b0e66549f5a
Muf Fie <joko@bilkoek.com>
2023-05-29 15:34:09 +1000

    added location

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,24 @@ import './App.css';
 import axios from "axios"
 
 function App() {
+
+  const [weather, setWeather] = React.useState("");
+  const [location,setLocation] = React.useState("");
+
   React.useEffect(() => {
-    axios.get("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=fe4feefa8543e06d4f3c66d92c61b69c").then((response) => {
+    axios.get("https://api.openweathermap.org/data/2.5/weather?lat=42.9832406&lon=-81.243372&appid=fe4feefa8543e06d4f3c66d92c61b69c").then((response) => {
       console.log(response.data);
+      console.log(response.data.weather[0].description);
+      setWeather(response.data.weather[0].description)
+      setLocation(response.data.name)
     });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+        The forecast for the weather in {location} for today is:
+       {weather} 
        
       </header>
     </div>
