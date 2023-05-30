3c30916962ba1a34bd1ee4d067c1f6860a1a2e4b
Muf Fie <joko@bilkoek.com>
2023-05-30 16:10:18 +1000

    api working for few cities

--- src/App.tsx
+++ src/App.tsx
@@ -14,52 +14,84 @@ import ico from './sun_icon.jpg'
 function App() {
 
   const [weather, setWeather] = React.useState("");
-  const [location, setLocation] = React.useState("");
+  const [location, setLocation] = React.useState("lat=48.8588897&lon=2.3200410217200766");
 
+  const [city, setCity] = React.useState("Paris");
 
   //lat=42.9832406&lon=-81.243372
 
-// const selectChangeHandler = (e:React.ChangeEvent<HTMLInputElement>) => 
-// {
-//   switch (e.currentTarget.value){
-//     case "London":
-//       setLocation("lat=42.9832406&lon=-81.243372")
-//   }
-// }
+  // const selectChangeHandler = (e:React.ChangeEvent<HTMLInputElement>) => 
+  // {
+  //   switch (e.currentTarget.value){
+  //     case "London":
+  //       setLocation("lat=42.9832406&lon=-81.243372")
+  //   }
+  // }
 
-const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-  const value = event.target.value;
-  console.log(value)
-  //setSelectedOption(value);
-};
+  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    console.log(value)
+    setLocation(value);
+    switch (value) {
+      case "London":
+        setLocation("lat=42.9832406&lon=-81.243372")
+        setCity("London")
+        break;
 
-  // React.useEffect(() => {
-  //   axios.get("https://api.openweathermap.org/data/2.5/weather?"+location+"&appid=fe4feefa8543e06d4f3c66d92c61b69c").then((response) => {
-  //     console.log(response.data);
-  //     //console.log(response.data.weather[0].description);
-  //     setWeather(response.data.weather[0].description)
-  //     setLocation(response.data.name)
-  //   });
-  // }, []);
+      case "Paris":
+        setLocation("lat=48.8588897&lon=2.3200410217200766")
+        setCity("Paris")
+        break;
+
+      case "Sydney":
+        setLocation("lat=-33.867926&lon=151.21013810170962")
+        setCity("Sydney")
+        break;
+
+      case "Tokyo":
+        setLocation("lat=35&lon=139")
+        setCity("Tokyo")
+        break;
+
+      case "Amsterdam":
+        setLocation("lat=38&lon=-94")
+        setCity("Amsterdam")
+        break;
+
+
+
+    }
+  };
+
+  React.useEffect(() => {
+    axios.get("https://api.openweathermap.org/data/2.5/weather?" + location + "&appid=fe4feefa8543e06d4f3c66d92c61b69c").then((response) => {
+      console.log(response.data);
+      console.log('useeffect')
+      //console.log(response.data.weather[0].description);
+      setWeather(response.data.weather[0].description)
+      //setLocation(response.data.name)
+    });
+  }, [location]);
 
   return (
     <Body>
       <Div>
         <Label>City</Label>
         <select onChange={selectChange}>
-          <option value="London">London</option>
           <option value="Paris">Paris</option>
+          <option value="London">London</option>
           <option value="Sydney">Sydney</option>
           <option value="Amsterdam">Amsterdam</option>
+          <option value="Tokyo">Tokyo</option>
         </select>
-       
 
-        <Div>The forecast for the weather in {location} for today is:
+
+        <Div>The forecast for the weather in {city} for today is:
           {weather} </Div>
         <Div>Degrees:</Div>
         <Div>Wind:</Div>
         <img width="200" height="300" src={ico}></img>
-        
+
 
       </Div>
 
